b2078fc5229abf9017c840f52d6d68880bcb1d75
AE <poron_ball@mail.ru>
2022-11-07 18:57:32 +0300

    test

--- test_lusky_ticket.py
+++ test_lusky_ticket.py
@@ -1,9 +1,9 @@
 from main import lucky_ticket
 
 def test(ticket):
-    arg1 = lucky_ticket(ticket)
-
-    assert agr1 == True
+    arg1 = True
+    print(arg1)
+    assert  lucky_ticket(ticket) == arg1
 
 
 test(123321)
